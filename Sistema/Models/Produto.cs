6c0b2ce0d6466c0aa4b6f27ea0f103b3750fd5e7
Guilherme da Luz <guilherme.daluz@outlook.com>
2017-07-26 14:03:03 -0300

    Model Produto

--- Sistema/Models/Produto.cs
+++ Sistema/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,15 @@ namespace Sistema.Models
 {
     public class Produto
     {
+        [Key]
+        public int ID { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public DateTime UltimaCompra { get; set; }
+
+        public float Estoque { get; set; }
     }
 }
